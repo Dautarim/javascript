8f06d0f3bb8050ebc2cd67a4ce13129cc2a53051
Dautarim Osvaldo Carvalho Andrelino <carvalhodeandrelino@gmail.com>
2022-06-02 17:39:40 -0300

    ljhsbkj

---
+++ lacerda/exc1/menufuncoes.js
@@ -0,0 +1,15 @@
+function abremenu()
+{
+    let ass = document.getElementById('paises')
+    let sand = document.getElementById('abrepernas')
+    
+    if (ass.classList=true)
+    {
+        ass.classList.add('baixos')
+    }
+    else
+    {
+        ass.classList.remove('baixos')
+    }
+    
+}

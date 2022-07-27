d938a3eea8fb1f1d6e1496b9a0baaeeb11423c72
Dautarim Osvaldo Carvalho Andrelino <carvalhodeandrelino@gmail.com>
2022-07-27 20:42:28 -0300

    d dsfmsz

--- lacerda/exercicosobreSPLIT/split.js
+++ lacerda/exercicosobreSPLIT/split.js
@@ -17,8 +17,8 @@ console.log(fazerdordearrows)
 
 //para conseguir o efeito contrário a isso (fazer um array ser string) seria com o metodo join, e o parametro seria para definir o que vai separar os elementos exemplo
 
-let arrayexemplo = ['vava', 'carlos', 'junior', 'elson', 'nelson', 'kelson']
-
+let arrayexemplo = ['vava', 'carlos', 'junior', 'elson', 'nelson', 'kelson','vava', 'carlos', 'junior', 'elson', 'nelson', 'kelson']
+let arrayexemplo2 = ['vava', 'carlos', 'junior', 'elson', 'nelson', 'kelson','vava', 'carlos', 'junior', 'elson', 'nelson', 'kelson']
 //agora vamos juntar
 
 let juntadordearray = arrayexemplo.join('**') //e se tiver preguiça de colocar parametro, pode susbstituir o join por toString(), que vai fazer a mesma coisa, mas vai separar por virgula automaticamente
@@ -32,3 +32,21 @@ console.log(juntadordearray)
 arrayexemplo[6] = 'ana'
 
 //é assim também que se altera algum elemento já existente do array
+//e para eliminar um elemento no array em qualquer posição usamos o splice(a posição do elemento ; o numero de elementos que vai ser eliminado apartir dele). Ex:
+//arrayexemplo.splice(1,2)
+
+//nesse caso, começamos apagamento no elemento na posição 1, e apagamos 2 elemetos , pode-se usar apenas um parametro, caso queira limpar todos os elementos a partir do elemento selecionado. Por exemplo splice(1)
+
+//o metodo delete() deleta o elemento mas deixa o seu esopaço vaziu no array
+//delete arrayexemplo[1]
+
+//para juntar dois arrays usamos o this.concat(nome do outro array).Ex:
+//arrayexemplo.concat(arrayexemplo2)
+
+//para ordenar o array usamos o sort
+//arrayexemplo.sort()
+
+//e para inverter a ordem usamos o reverse
+//arrayexemplo.reverse()
+
+
